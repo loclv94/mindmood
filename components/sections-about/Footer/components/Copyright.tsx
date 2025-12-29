a33192f44d15134731c5604203b01db378e9de5d
loclv94 <lanlap.towork@gmail.com>
2025-12-29 20:11:36 +0700

    refactor: remove unused Footer components and related files

--- components/sections-about/Footer/components/Copyright.tsx
+++
@@ -1,7 +0,0 @@
-export const Copyright = () => {
-  return (
-    <span className="box-border caret-transparent clear-both block text-center">
-      ©Copyright 2025 MindMood.ai{" "}
-    </span>
-  );
-};
